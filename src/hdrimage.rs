8290411ce8dc9a6bcb32d7068b4dd300e3a1da6c
andros21 <andrea.ros21@e.email>
2022-03-16 09:58:28 +0100

    fix(hdrimage)!: construct `HdrImage` using `new` method
    BREAKING_CHANGE: no more `From` trait re-implementation
    
    tests: adding 2 new tests

--- src/hdrimage.rs
+++ src/hdrimage.rs
@@ -1,6 +1,6 @@
 use std::vec::Vec;
 
-use crate::color::Color;
+use crate::color::{Color, IsClose};
 use crate::error::HdrImageErr;
 
 #[derive(Clone, PartialEq)]
@@ -10,17 +10,15 @@ pub struct HdrImage {
     pixels: Vec<Color>,
 }
 
-impl From<(usize, usize)> for HdrImage {
-    fn from(shape: (usize, usize)) -> Self {
-        Self {
-            width: shape.0,
-            height: shape.1,
-            pixels: vec![Color::from(()); shape.0 * shape.1],
+impl HdrImage {
+    fn new(width: usize, height: usize) -> HdrImage {
+        HdrImage {
+            width,
+            height,
+            pixels: vec![Default::default(); width * height],
         }
     }
-}
 
-impl HdrImage {
     fn pixel_offset(&self, x: usize, y: usize) -> usize {
         y * self.width + x
     }
@@ -52,47 +50,56 @@ mod test {
     use super::*;
 
     #[test]
+    fn image_creation() {
+        let hdr_img = HdrImage::new(5, 7);
+        assert_eq!(hdr_img.width, 5);
+        assert_eq!(hdr_img.height, 7)
+    }
+
+    #[test]
     fn validate_coordinates_0() {
-        assert!(HdrImage::from((3, 3)).valid_coordinates(0, 0))
+        assert!(HdrImage::new(3, 3).valid_coordinates(0, 0))
     }
 
     #[test]
     fn validate_coordinates_1() {
-        assert!(HdrImage::from((3, 3)).valid_coordinates(1, 2))
+        assert!(HdrImage::new(3, 3).valid_coordinates(1, 2))
     }
 
     #[test]
     fn validate_coordinates_2() {
-        assert!(HdrImage::from((3, 3)).valid_coordinates(2, 1))
+        assert!(HdrImage::new(3, 3).valid_coordinates(2, 1))
     }
 
     #[test]
     fn neg_validate_coordinates_0() {
-        assert!(!HdrImage::from((3, 3)).valid_coordinates(3, 3))
+        assert!(!HdrImage::new(3, 3).valid_coordinates(3, 3))
     }
 
     #[test]
     fn neg_validate_coordinates_1() {
-        assert!(!HdrImage::from((3, 3)).valid_coordinates(3, 1))
+        assert!(!HdrImage::new(3, 3).valid_coordinates(3, 1))
     }
 
     #[test]
     fn neg_validate_coordinates_2() {
-        assert!(!HdrImage::from((3, 3)).valid_coordinates(1, 3))
+        assert!(!HdrImage::new(3, 3).valid_coordinates(1, 3))
     }
 
     #[test]
     fn get_pixel_ok() {
         assert_eq!(
-            HdrImage::from((3, 3)).get_pixel(0, 0).unwrap(),
-            Color::from(())
+            HdrImage::new(3, 3).get_pixel(0, 0).unwrap(),
+            Color {
+                ..Default::default()
+            }
         )
     }
 
     #[test]
     fn get_pixel_err() {
         assert_eq!(
-            HdrImage::from((3, 3)).get_pixel(3, 3).unwrap_err(),
+            HdrImage::new(3, 3).get_pixel(3, 3).unwrap_err(),
             HdrImageErr::OutOfBounds((3, 3), (3, 3))
         )
     }
@@ -100,19 +107,28 @@ mod test {
     #[test]
     fn set_pixel_ok() {
         let color = Color::from((1.0, 1.0, 1.0));
-        let mut hdrimage = HdrImage::from((3, 3));
-        assert_eq!(hdrimage.set_pixel(0, 0, color), Ok(()));
-        assert_eq!(hdrimage.get_pixel(0, 0).unwrap(), color)
+        let mut hdr_image = HdrImage::new(3, 3);
+        assert_eq!(hdr_image.set_pixel(0, 0, color), Ok(()));
+        assert_eq!(hdr_image.get_pixel(0, 0).unwrap(), color)
     }
 
     #[test]
     fn set_pixel_err() {
         let color = Color::from((1.0, 1.0, 1.0));
-        let mut hdrimage = HdrImage::from((3, 3));
+        let mut hdr_image = HdrImage::new(3, 3);
         assert_eq!(
-            hdrimage.set_pixel(3, 3, color).unwrap_err(),
+            hdr_image.set_pixel(3, 3, color).unwrap_err(),
             HdrImageErr::OutOfBounds((3, 3), (3, 3))
         );
-        assert_ne!(hdrimage.get_pixel(0, 0).unwrap(), color)
+        assert_ne!(hdr_image.get_pixel(0, 0).unwrap(), color)
+    }
+
+    #[test]
+    fn is_close_get_set_pixel() -> Result<(), HdrImageErr> {
+        let color = Color::from((1.2, 2.3, 3.4));
+        let mut hdr_img = HdrImage::new(5, 7);
+        hdr_img.set_pixel(1, 3, color)?;
+        hdr_img.get_pixel(1, 3)?.is_close(color);
+        Ok(())
     }
 }

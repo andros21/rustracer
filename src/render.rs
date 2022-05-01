bc22424884c33f9cc79808a78e2b9a8db86c017f
andros21 <andrea.ros21@e.email>
2022-05-01 11:51:50 +0200

    feat(render): add `OnOffRenderer`
    the first render eq. solver, it only produces
    black and white simple image
    
    docs missing

--- src/render.rs
+++ src/render.rs
@@ -3,6 +3,7 @@
 //! Provides different renderers that implement [`Solve`] trait.
 use crate::color::Color;
 use crate::ray::Ray;
+use crate::world::World;
 
 /// A trait for solving rendering equation.
 ///
@@ -10,4 +11,28 @@ use crate::ray::Ray;
 /// color to assign to a pixel in the image.
 pub trait Solve {
     fn solve(&self, ray: Ray) -> Color;
+}
+
+pub struct OnOffRenderer<'a> {
+    world: &'a World,
+    background_color: Color,
+    color: Color,
+}
+
+impl<'a> OnOffRenderer<'a> {
+    pub fn new(world: &World, background_color: Color, color: Color) -> OnOffRenderer {
+        OnOffRenderer {
+            world,
+            background_color,
+            color,
+        }
+    }
+}
+impl<'a> Solve for OnOffRenderer<'a> {
+    fn solve(&self, ray: Ray) -> Color {
+        match self.world.ray_intersection(ray) {
+            Some(_hit) => self.color,
+            None => self.background_color,
+        }
+    }
 }

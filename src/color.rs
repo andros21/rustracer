be61443f5faafcee2c619f29f71b67c85b8d493a
andros21 <andrea.ros21@e.email>
2022-05-01 11:25:52 +0200

    feat(color): add two useful constant colors
    black and white

--- src/color.rs
+++ src/color.rs
@@ -4,6 +4,17 @@
 use crate::misc::IsClose;
 use std::ops::{Add, Mul};
 
+pub const BLACK: Color = Color {
+    r: 0.0,
+    g: 0.0,
+    b: 0.0,
+};
+pub const WHITE: Color = Color {
+    r: 1.0,
+    g: 1.0,
+    b: 1.0,
+};
+
 /// RGB Color struct.
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub struct Color {

23879ff4d170d9c1e3efec631e7a741c8159e427
andros21 <andrea.ros21@e.email>
2022-05-23 12:28:45 +0200

    feat(pathtracer): new demo command

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,19 @@ mod world;
 
 use image::ImageFormat;
 use std::env;
+use std::f32::consts::PI;
 use std::path::Path;
 use std::process::exit;
 use std::str::FromStr;
 
 use crate::camera::{OrthogonalCamera, PerspectiveCamera};
-use crate::color::{BLACK, WHITE};
+use crate::color::{Color, BLACK, WHITE};
 use crate::error::{ConvertErr, DemoErr, HdrImageErr};
 use crate::hdrimage::{HdrImage, Luminosity};
 use crate::imagetracer::ImageTracer;
-use crate::material::Material;
+use crate::material::{
+    CheckeredPigment, DiffuseBRDF, Material, Pigment, SpecularBRDF, UniformPigment, BRDF,
+};
 use crate::misc::ByteOrder;
 use crate::render::OnOffRenderer;
 use crate::shape::Sphere;
@@ -90,29 +93,69 @@ fn demo(sub_m: &clap::ArgMatches) -> Result<(), DemoErr> {
         .map_err(|e| DemoErr::IntParseFailure(e, String::from("height")))?;
     let angle_deg = f32::from_str(sub_m.value_of("angle-deg").unwrap())
         .map_err(|e| DemoErr::FloatParseFailure(e, String::from("angle-deg")))?;
+    let algorithm = Path::new(sub_m.value_of("algorithm").unwrap());
+    let num_of_rays = u32::from_str(sub_m.value_of("num-of-rays").unwrap())
+        .map_err(|e| DemoErr::IntParseFailure(e, String::from("num-of-rays")))?;
+    let max_depth = u32::from_str(sub_m.value_of("max-depth").unwrap())
+        .map_err(|e| DemoErr::IntParseFailure(e, String::from("max-depth")))?;
+    let init_state = u32::from_str(sub_m.value_of("init-state").unwrap())
+        .map_err(|e| DemoErr::IntParseFailure(e, String::from("init-state")))?;
+    let init_seq = u32::from_str(sub_m.value_of("init-seq").unwrap())
+        .map_err(|e| DemoErr::IntParseFailure(e, String::from("init-seq")))?;
+    let samples_per_pixel = u32::from_str(sub_m.value_of("samples-per-pixel").unwrap())
+        .map_err(|e| DemoErr::IntParseFailure(e, String::from("samples-per-pixel")))?;
     check!(ldr_file).map_err(DemoErr::IoError)?;
+    let sky_material = Material {
+        brdf: BRDF::Diffuse(DiffuseBRDF {
+            pigment: Pigment::Uniform(UniformPigment::default()),
+        }),
+        emitted_radiance: Pigment::Uniform(UniformPigment {
+            color: Color::from((1.0, 0.9, 0.5)),
+        }),
+    };
+    let ground_material = Material {
+        brdf: BRDF::Diffuse(DiffuseBRDF {
+            pigment: Pigment::Checkered(CheckeredPigment {
+                color1: Color::from((0.3, 0.5, 0.1)),
+                color2: Color::from((0.1, 0.2, 0.5)),
+                steps: 10,
+            }),
+        }),
+        emitted_radiance: Pigment::Uniform(UniformPigment::default()),
+    };
+    let sphere_material = Material {
+        brdf: BRDF::Diffuse(DiffuseBRDF {
+            pigment: Pigment::Uniform(UniformPigment {
+                color: Color::from((0.3, 0.4, 0.8)),
+            }),
+        }),
+        emitted_radiance: Pigment::Uniform(UniformPigment::default()),
+    };
+    let mirror_material = Material {
+        brdf: BRDF::Specular(SpecularBRDF {
+            pigment: Pigment::Uniform(UniformPigment {
+                color: Color::from((0.6, 0.2, 0.3)),
+            }),
+            threshold_angle_rad: PI / 1800.0,
+        }),
+        emitted_radiance: Pigment::Uniform(UniformPigment::default()),
+    };
     let mut hdr_img = HdrImage::new(width, height);
     if sub_m.is_present("verbose") {
         println!("[info] generating an image ({}, {})", width, height);
     }
     let mut world = World::default();
-    for x in [-0.5, 0.5].into_iter() {
-        for y in [-0.5, 0.5].into_iter() {
-            for z in [-0.5, 0.5].into_iter() {
-                world.add(Box::new(Sphere::new(
-                    translation(Vector::from((x, y, z))) * scaling(Vector::from((0.1, 0.1, 0.1))),
-                    Material::default(),
-                )));
-            }
-        }
-    }
     world.add(Box::new(Sphere::new(
-        translation(Vector::from((0.0, 0.0, -0.5))) * scaling(Vector::from((0.1, 0.1, 0.1))),
-        Material::default(),
+        translation(Vector::from((0.0, 0.0, 0.4))) * scaling(Vector::from((200.0, 200.0, 200.0))),
+        sky_material,
     )));
     world.add(Box::new(Sphere::new(
-        translation(Vector::from((0.0, 0.5, 0.0))) * scaling(Vector::from((0.1, 0.1, 0.1))),
-        Material::default(),
+        translation(Vector::from((0.0, 0.0, 0.1))),
+        sphere_material,
+    )));
+    world.add(Box::new(Sphere::new(
+        translation(Vector::from((1.0, 2.5, 0.0))),
+        mirror_material,
     )));
     let camera_tr =
         rotation_z(f32::to_radians(angle_deg)) * translation(Vector::from((-1.0, 0.0, 0.0)));

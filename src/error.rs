d3a974cc89141ee204595fe80839cc58f51d3e97
andros21 <andrea.ros21@e.email>
2022-06-18 15:07:28 +0200

    feat(error): colored output

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@
 //! using [`thiserror`](https://github.com/dtolnay/thiserror) library.
 
 use crate::scene::SourceLocation;
+use colored::Colorize;
 use thiserror::Error;
 
 /// Error enum for [`HdrImage`](../hdrimage) module.
@@ -13,58 +14,71 @@ pub enum HdrImageErr {
     OutOfBounds((u32, u32), (u32, u32)),
     #[error("invalid pixels vector size: {0}, expected size: {1}")]
     InvalidPixelsSize(u32, u32),
-    #[error("invalid pfm file format: {0}")]
+    #[error("invalid pfm file format: {0:?}")]
     InvalidPfmFileFormat(String),
-    #[error("impossible to read from pfm file: {0}")]
+    #[error("impossible to read from pfm file\n\tsource: {}",
+        format!("{}", .0).to_lowercase())]
     PfmFileReadFailure(#[source] std::io::Error),
-    #[error("impossible to write to pfm file: {0}")]
+    #[error("impossible to write to pfm file\n\tsource: {}",
+        format!("{}", .0).to_lowercase())]
     PfmFileWriteFailure(#[source] std::io::Error),
-    #[error("impossible to parse {1} as integer from pfm file: {0}")]
+    #[error("impossible to parse {1} as integer from pfm file\n\tsource: {}",
+        format!("{}", .0).to_lowercase())]
     PfmIntParseFailure(#[source] std::num::ParseIntError, String),
-    #[error("impossible to parse {1} as float from pfm file: {0}")]
+    #[error("impossible to parse {1} as float from pfm file\n\tsource: {}",
+        format!("{}", .0).to_lowercase())]
     PfmFloatParseFailure(#[source] std::num::ParseFloatError, String),
-    #[error("impossible to write to ldr file: {0}")]
+    #[error("impossible to write to ldr file\n\tsource: {}",
+        format!("{}", .0).to_lowercase())]
     LdrFileWriteFailure(#[source] image::ImageError),
-    #[error("unsupported {0} ldr file format, only ff or png supported")]
+    #[error("unsupported {0:?} ldr file format, only \"ff\" or \"png\" supported")]
     UnsupportedLdrFileFormat(String),
 }
 
 /// Error enum for [`convert`](../fn.convert.html) function inside [`main`](../fn.main.html).
 #[derive(Error, Debug)]
 pub enum ConvertErr {
-    #[error("invalid {1}, expected floating-point number: {0}")]
+    #[error("{}\n\tsource: {0}",
+        format!("{:?} flag invalid value, expected floating-point number", .1).bold())]
     FloatParseFailure(#[source] std::num::ParseFloatError, String),
-    #[error("{0}")]
+    #[error("{}\n\tsource: {0}", "image convert input/output error".bold())]
     IoError(#[source] HdrImageErr),
 }
 
 /// Error enum for [`demo`](../fn.demo.html) function inside [`main`](../fn.main.html).
 #[derive(Error, Debug)]
 pub enum DemoErr {
-    #[error("invalid {1}, expected integer number: {0}")]
+    #[error("{}\n\tsource: {0}",
+        format!("{:?} flag invalid value, expected integer number", .1).bold())]
     IntParseFailure(#[source] std::num::ParseIntError, String),
-    #[error("invalid {1}, expected floating-point number: {0}")]
+    #[error("{}\n\tsource: {0}",
+        format!("{:?} flag invalid value, expected floating-point number", .1).bold())]
     FloatParseFailure(#[source] std::num::ParseFloatError, String),
-    #[error("{0}")]
+    #[error("{}\n\tsource: {0}", "demo render input/output error".bold())]
     IoError(#[source] HdrImageErr),
 }
 
 /// Error enum for [`Scene`](../scene) module.
 #[derive(Error, Debug)]
 pub enum SceneErr {
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     InvalidCharacter { loc: SourceLocation, msg: String },
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     UnclosedString { loc: SourceLocation, msg: String },
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     FloatParseFailure {
         loc: SourceLocation,
         msg: String,
         src: std::num::ParseFloatError,
     },
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     NotMatch { loc: SourceLocation, msg: String },
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}\n\tsource: {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg, src)]
     PfmFileReadFailure {
         loc: SourceLocation,
         msg: String,
@@ -72,23 +86,33 @@ pub enum SceneErr {
     },
     #[error("{0}")]
     UnexpectedMatch(String),
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     UndefinedIdentifier { loc: SourceLocation, msg: String },
-    #[error("<{}:{}> {}", .loc.line_num, .loc.col_num, msg)]
+    #[error("{} {}",
+        format!(":{}:{}", loc.line_num, loc.col_num).yellow(), msg)]
     InvalidCamera { loc: SourceLocation, msg: String },
-    #[error("impossible to read from scene file: {0}")]
+    #[error("{} impossible to read from scene file\n\tsource: {0}", "::".yellow())]
     SceneFileReadFailure(#[source] std::io::Error),
 }
 
 /// Error enum for [`render`](../fn.render.html) function inside [`main`](../fn.main.html).
 #[derive(Error, Debug)]
 pub enum RenderErr {
-    #[error("invalid {1}, expected integer number: {0}")]
+    #[error("{}\n\tsource: {0}",
+        format!("{:?} flag invalid value, expected integer number", .1).bold())]
     IntParseFailure(#[source] std::num::ParseIntError, String),
-    #[error("invalid {1}, expected floating-point number: {0}")]
+    #[error("{}\n\tsource: {0}",
+        format!("{:?} flag invalid value, expected floating-point number", .1).bold())]
     FloatParseFailure(#[source] std::num::ParseFloatError, String),
-    #[error("{0}")]
+    #[error("{}\n\tsource: {0}", "render input/output error".bold())]
     IoError(#[source] HdrImageErr),
-    #[error("({1}) {0}")]
+    #[error("{}\n\tsource: {}",
+        format!("render parsing scene from {:?}", .1).bold(),
+        format!("{file}{src}",
+            file = std::path::Path::new(.1).file_name()
+                                           .unwrap_or_else(|| std::ffi::OsStr::new(.1))
+                                           .to_str().unwrap().yellow(),
+            src = .0))]
     SceneError(#[source] SceneErr, String),
 }

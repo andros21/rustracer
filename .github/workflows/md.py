5a2b9a99f66c41f560a836df9203157c1cd1127e
andros21 <andrea.ros21@e.email>
2022-07-15 11:33:51 +0200

    build(ci): add update/generate README script
    from README template

---
+++ .github/workflows/md.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# md.py
+# =========
+# update `README.md` console help output from `TEMPLATE.md`
+
+import subprocess
+
+from jinja2 import Template
+
+if __name__ == "__main__":
+    rustracer = {
+        "rustracer": subprocess.run(
+            "target/debug/rustracer -h".split(), capture_output=True, encoding="utf8"
+        ).stdout,
+        "rustracer_convert": subprocess.run(
+            "target/debug/rustracer convert -h".split(),
+            capture_output=True,
+            encoding="utf8",
+        ).stdout,
+        "rustracer_demo": subprocess.run(
+            "target/debug/rustracer demo -h".split(),
+            capture_output=True,
+            encoding="utf8",
+        ).stdout,
+        "rustracer_render": subprocess.run(
+            "target/debug/rustracer render -h".split(),
+            capture_output=True,
+            encoding="utf8",
+        ).stdout,
+        "rustracer_completion": subprocess.run(
+            "target/debug/rustracer completion -h".split(),
+            capture_output=True,
+            encoding="utf8",
+        ).stdout,
+    }
+
+    with open("TEMPLATE.md") as ifs:
+        template = Template(ifs.read())
+    with open("README.md", "w") as ofs:
+        ofs.write(template.render(rustracer) + "\n")

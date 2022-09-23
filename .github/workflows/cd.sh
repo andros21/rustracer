a97e26f3ead3ad3864220003558f46298fb8fcd4
andros21 <andrea.ros21@e.email>
2022-09-23 16:02:18 +0200

    reformat(cd.yml/cd.sh): compact same tasks for multiple platforms
    now that multiple `--target` can be specified and bins for
    default platform are placed inside `target/$default-platform`
    see https://github.com/rust-lang/rust/releases/tag/1.64.0

--- .github/workflows/cd.sh
+++ .github/workflows/cd.sh
@@ -7,31 +7,32 @@
 
 set -e
 
-GNU="rustracer-${GITHUB_REF_NAME}-x86_64-unknown-linux-gnu"
-MUSL="rustracer-${GITHUB_REF_NAME}-x86_64-unknown-linux-musl"
-SUM="rustracer-${GITHUB_REF_NAME}_checksums_sha256.txt"
+PLATFORMS=("x86_64-unknown-linux-gnu" "x86_64-unknown-linux-musl")
+BASENAME="rustracer-${GITHUB_REF_NAME}"
+CHECKSUM="rustracer-${GITHUB_REF_NAME}_checksums_sha256.txt"
 
 _assemble() {
-   mkdir -pv "${GNU}/bin"
-   mkdir -pv "${MUSL}/bin"
-   cp -v target/release/rustracer \
-      "${GNU}/bin/rustracer"
-   cp -v target/x86_64-unknown-linux-musl/release/rustracer \
-      "${MUSL}/bin/rustracer"
-   cp -v {README.md,LICENSE} "${GNU}/"
-   cp -v {README.md,LICENSE} "${MUSL}/"
-   tar czfv "${GNU}.tar.gz" "${GNU}"
-   tar czfv "${MUSL}.tar.gz" "${MUSL}"
-   sha256sum ./*.tar.gz > "${SUM}"
+   for PLATFORM in "${PLATFORMS[@]}"
+   do
+      mkdir -pv "${BASENAME}-${PLATFORM}/bin"
+      cp -v "target/${PLATFORM}/release/rustracer" \
+         "${BASENAME}-${PLATFORM}/bin/rustracer"
+      cp -v {README.md,LICENSE} "${BASENAME}-${PLATFORM}/"
+      tar czfv "${BASENAME}-${PLATFORM}.tar.gz" "${BASENAME}-${PLATFORM}"
+   done
+   sha256sum ./*.tar.gz > "${CHECKSUM}"
 }
 
 _cosign() {
-   for blob in "${GNU}.tar.gz" "${MUSL}.tar.gz" "${SUM}"
+   for PLATFORM in "${PLATFORMS[@]}"
    do
-      cosign sign-blob "${blob}" \
-         --output-signature "${blob}-keyless.sig" \
-         --output-certificate "${blob}-keyless.pem"
+      cosign sign-blob "${BASENAME}-${PLATFORM}.targ.gz" \
+         --output-signature "${BASENAME}-${PLATFORM}-keyless.sig" \
+         --output-certificate "${BASENAME}-${PLATFORM}-keyless.pem"
    done
+   cosign sign-blob "${CHECKSUM}" \
+      --output-signature "${CHECKSUM}-keyless.sig" \
+      --output-certificate "${CHECKSUM}-keyless.pem"
 }
 
 case $1 in

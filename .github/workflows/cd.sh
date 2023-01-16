andros21/rustracer

commit 528ea0703a61e3e5abc80b28959cc9646c481215

reformat(cd.yml/cd.sh): compact same tasks for multiple platforms (#115)
* build(Cargo.toml): bump rust from 1.62 to 1.64

* reformat(cd.yml/cd.sh): compact same tasks for multiple platforms

now that multiple `--target` can be specified and bins for
default platform are placed inside `target/$default-platform`
see https://github.com/rust-lang/rust/releases/tag/1.64.0

.github/workflows/cd.sh
@@ -7,31 +7,32 @@
 
 set -e
 
-GNU="rustracer-${GITHUB_REF_NAME}-x86_64-unknown-linux-gnu"
-MUSL="rustracer-${GITHUB_REF_NAME}-x86_64-unknown-linux-musl"
-SUM="rustracer-${GITHUB_REF_NAME}_checksums_sha256.txt"
+PLATFORMS=("x86_64-unknown-linux-gnu" "x86_64-unknown-linux-musl")
+BASENAME="rustracer-${GITHUB_REF_NAME}"
+CHECKSUM="rustracer-${GITHUB_REF_NAME}_checksums_sha256.txt"
 
 _assemble() {
-   mkdir -pv "${GNU}/bin"
-   mkdir -pv "${MUSL}/bin"
-   cp -v target/release/rustracer \
-      "${GNU}/bin/rustracer"
-   cp -v target/x86_64-unknown-linux-musl/release/rustracer \
-      "${MUSL}/bin/rustracer"
-   cp -v {README.md,LICENSE} "${GNU}/"
-   cp -v {README.md,LICENSE} "${MUSL}/"
-   tar czfv "${GNU}.tar.gz" "${GNU}"
-   tar czfv "${MUSL}.tar.gz" "${MUSL}"
-   sha256sum ./*.tar.gz > "${SUM}"
+   for PLATFORM in "${PLATFORMS[@]}"
+   do
+      mkdir -pv "${BASENAME}-${PLATFORM}/bin"
+      cp -v "target/${PLATFORM}/release/rustracer" \
+         "${BASENAME}-${PLATFORM}/bin/rustracer"
+      cp -v {README.md,LICENSE} "${BASENAME}-${PLATFORM}/"
+      tar czfv "${BASENAME}-${PLATFORM}.tar.gz" "${BASENAME}-${PLATFORM}"
+   done
+   sha256sum ./*.tar.gz > "${CHECKSUM}"
 }
 
 _cosign() {
-   for blob in "${GNU}.tar.gz" "${MUSL}.tar.gz" "${SUM}"
+   for PLATFORM in "${PLATFORMS[@]}"
    do
-      cosign sign-blob "${blob}" \
-         --output-signature "${blob}-keyless.sig" \
-         --output-certificate "${blob}-keyless.pem"
+      cosign sign-blob "${BASENAME}-${PLATFORM}.targ.gz" \
+         --output-signature "${BASENAME}-${PLATFORM}-keyless.sig" \
+         --output-certificate "${BASENAME}-${PLATFORM}-keyless.pem"
    done
+   cosign sign-blob "${CHECKSUM}" \
+      --output-signature "${CHECKSUM}-keyless.sig" \
+      --output-certificate "${CHECKSUM}-keyless.pem"
 }
 
 case $1 in

.github/workflows/cd.yml
@@ -33,16 +33,11 @@ jobs:
           default: true
           profile: minimal
           target: x86_64-unknown-linux-musl
-      - name: cargo release gnu
+      - name: cargo release
         uses: actions-rs/cargo@844f36862e911db73fe0815f00a4a2602c279505
         with:
           command: build
-          args: --locked --release
-      - name: cargo release musl
-        uses: actions-rs/cargo@844f36862e911db73fe0815f00a4a2602c279505
-        with:
-          command: build
-          args: --locked --release --target x86_64-unknown-linux-musl
+          args: --locked --release --target x86_64-unknown-linux-gnu --target x86_64-unknown-linux-musl
       - name: assemble artifacts
         run: .github/workflows/cd.sh assemble
       - name: check cosign version

Cargo.toml
@@ -3,7 +3,7 @@ name         = "rustracer"
 license      = "GPL-3.0"
 version      = "1.0.1"
 edition      = "2021"
-rust-version = "1.62"
+rust-version = "1.64"
 authors      = [
    "Andrea Rossoni <andrea dot ros.21 at e.email>",
    "Paolo Azzini <paolo dot azzini1 at gmail.com>"

commit efe72d865cc5f77b5774e4490d89853ac4b8f01e

build(cd.sh): fix file extension in release script
error introduced in https://github.com/andros21/rustracer/pull/115

.github/workflows/cd.sh
@@ -26,7 +26,7 @@ _assemble() {
 _cosign() {
    for PLATFORM in "${PLATFORMS[@]}"
    do
-      cosign sign-blob "${BASENAME}-${PLATFORM}.targ.gz" \
+      cosign sign-blob "${BASENAME}-${PLATFORM}.tar.gz" \
          --output-signature "${BASENAME}-${PLATFORM}-keyless.sig" \
          --output-certificate "${BASENAME}-${PLATFORM}-keyless.pem"
    done